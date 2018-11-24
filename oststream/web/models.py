4db3fa038f6b3a849ea0a935a4610d6b93d92b7e
wTheRockb <forrestwbutler@gmail.com>
2018-11-24 13:50:56 -0500

    models printing and admin working

---
+++ oststream/web/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+import json
+from django.forms.models import model_to_dict
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    # password todo?
+
+    def __str__(self):
+        return json.dumps(model_to_dict(self))
+
+
+class GameSeries(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return json.dumps(model_to_dict(self))
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    series = models.ForeignKey(GameSeries, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return json.dumps(model_to_dict(self))
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    publish_date = models.DateTimeField('date published')
+    # Consider making this slugfield https://docs.djangoproject.com/en/2.1/ref/models/fields/#slugfield
+    image_path = models.CharField(max_length=200)
+
+    def __str__(self):
+        return json.dumps(model_to_dict(self))
+
+
+class Track(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return json.dumps(model_to_dict(self))
+
+
+class Playlist(models.Model):
+    title = models.CharField(max_length=200)
+    songs = models.ManyToManyField(Track, through='PlaylistMembership')
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return json.dumps(model_to_dict(self))
+
+
+class PlaylistMembership(models.Model):
+    track_id = models.ForeignKey(Track, on_delete=models.CASCADE)
+    playlist_id = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    date_added = models.DateField()
+
+    def __str__(self):
+        return json.dumps(model_to_dict(self))
+
+
+# TODO: RecentlyPlayed / Play history
