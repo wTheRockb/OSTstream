3798e3df83f730b50b3ac1c313b914a69ab4483c
wTheRockb <forrestwbutler@gmail.com>
2019-03-24 15:48:08 -0400

    worked on componetn a atad

--- js/oststream/src/components/album/Album.tsx
+++ js/oststream/src/components/album/Album.tsx
@@ -3,5 +3,14 @@ import * as React from 'react';
 export interface DisplayAlbumProps {
     name: string;
     album_cover_uri: string;
-    
+    game_series: string;
+}
+
+const DisplayAlbum: React.SFC<DisplayAlbumProps> = props => {
+
+    return (
+        <div className="displayAlbum">
+            helo i am album
+        </div>
+    )
 }
