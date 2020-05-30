8b3d0da39eb0e2ee38f5493463967314b2ceda14
wTheRockb <forrestwbutler@gmail.com>
2020-05-30 17:46:22 -0400

    update table styling and add icon

--- js/oststream/src/components/AlbumTrackList/AlbumTrackList.tsx
+++ js/oststream/src/components/AlbumTrackList/AlbumTrackList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "./style.scss";
 import { Track } from "src/types/Track";
+import timer from 'src/img/timer.svg'
 
 interface AlbumTrackListProps {
   readonly tracks: Track[];
@@ -11,12 +12,12 @@ export const AlbumTrackList: React.FunctionComponent<AlbumTrackListProps> = (
 ) => {
   return (
     <div className="album-track-list">
-      <table>
+      <table className="album-track-list__table">
         <tbody>
           <tr className="album-track-list__header">
             <th className="album-track-list__header-SONG"> SONG </th>
             <th className="album-track-list__header-ARTIST"> ARTIST </th>
-            <th className="album-track-list__header-TIME"> TIME </th>
+            <th className="album-track-list__header-TIME"> <img src={timer}/> </th>
           </tr>
           {props.tracks.map((track) => {
             return (
