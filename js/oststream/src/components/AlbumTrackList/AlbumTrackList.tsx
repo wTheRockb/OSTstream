ca40d3dcc3ba2f733e4e629e310f0aba3622239c
wTheRockb <forrestwbutler@gmail.com>
2020-05-31 23:42:14 -0400

    can't get svg to load

--- js/oststream/src/components/AlbumTrackList/AlbumTrackList.tsx
+++ js/oststream/src/components/AlbumTrackList/AlbumTrackList.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "./style.scss";
 import { Track } from "src/types/Track";
-import timer from 'src/img/timer.svg'
+// import TimerSvg from './timer.svg';
 
 interface AlbumTrackListProps {
   readonly tracks: Track[];
@@ -17,17 +17,17 @@ export const AlbumTrackList: React.FunctionComponent<AlbumTrackListProps> = (
           <tr className="album-track-list__header">
             <th className="album-track-list__header-SONG"> SONG </th>
             <th className="album-track-list__header-ARTIST"> ARTIST </th>
-            <th className="album-track-list__header-TIME"> <img src={timer}/> </th>
+            <th className="album-track-list__header-TIME"> 
+            {/* <TimerSvg/> */}
+            TIME
+             </th>
           </tr>
           {props.tracks.map((track) => {
             return (
-              <tr className="album-track-list__row">
-                <td className="album-track-list__row-SONG"> {track.title}</td>
-                <td className="album-track-list__row-ARTIST">
-                  {" "}
-                  {track.artist}{" "}
-                </td>
-                <td className="album-track-list__row-TIME"> {track.length}</td>
+              <tr className="album-track-list__row" key={track.id}>
+                <td className="album-track-list__row-SONG">{track.title}</td>
+                <td className="album-track-list__row-ARTIST">{track.artist}</td>
+                <td className="album-track-list__row-TIME">{track.length}</td>
               </tr>
             );
           })}
