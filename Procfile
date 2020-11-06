6c48177b9127d74e25fd7de9eb8d6137fdd598a9
Woody Butler <forrestwbutler@gmail.com>
2020-11-06 11:25:34 -0800

    Prep for heroku deploys

---
+++ Procfile
@@ -0,0 +1,2 @@
+client: serve -s build js/server.js
+api: python oststream/manage.py runserver 0.0.0.0:5000
--- js/src/components/Player/player.tsx
+++ js/src/components/Player/player.tsx
@@ -11,7 +11,6 @@ interface SongPlayerProps {
 }
 
 const SongPlayer: React.FunctionComponent<SongPlayerProps> = (props) => {
-    console.log(props)
 
     return (
         <div className="player__root">
